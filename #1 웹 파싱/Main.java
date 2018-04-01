ed8aff3db1c83a727501371af4f3ebbff5e8b25f
swk3169 <swk3169@gmail.com>
2018-04-01 22:01:40 +0900

    2018.04.01

--- #1 웹 파싱/Main.java
+++ #1 웹 파싱/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -10,12 +11,13 @@ public class Main
 		String target = "http://www.president.go.kr/";
 		HttpURLConnection con = (HttpURLConnection) new URL(target).openConnection();
 		BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+		FileWriter fw = null;
 		String temp;
 		while((temp = br.readLine()) != null)
 		{
-			if(temp.contains("rel"))
+			if(temp.contains("class=\"fullheight\""))
 			{
-				System.out.println(temp);
+				System.out.println(temp.split("rel=\"")[1].split("\"")[0]);
 			}
 		}
 		con.disconnect();
